23976ee0cae23c7c2ff2c755c721fcf15ab35f8d
Brandon Henning <prescott.henning@gmail.com>
2018-08-29 14:10:38 -0600

    a

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/app/index.html',
-      filename: './index.html'
+      filename: 'index.html'
     })
   ]
 };
